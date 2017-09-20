59850590e2e5e7b59c687458024cd2b279544b50
lucianturk <31940651+lucianturk@users.noreply.github.com>
2017-09-20 18:32:00 -0500

    function prototypes

--- src/Payroll.h
+++ src/Payroll.h
@@ -1,9 +1,11 @@
 #ifndef _PAYROLL_H
 #define _PAYROLL_H
 
-double getOvertimeHours(double totalHours);
-double getRegularHours(double totalHours);
-double getGrossPay(double regularHours, double overtimeHours, double hourlyRate);
-double getGrossPay(double salary);
+double get_reg_hours(double total_hours);
+double get_ot_hours(double total_hours);
+double get_gross_pay(double reg_hours, double ot_hours, double hourly_rate);
+double get_gross_pay(double salary);
+void show_check_hourly(double gross_pay, double reg_hours, double ot_hours);
+void show_check_salary(double gross_pay);
 
 #endif
